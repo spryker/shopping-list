9a5201e888d54edd39c0dff4b49e89d9dffd1b82
Sergey Samoylov <sergey.samoylov@spryker.com>
2019-02-12 19:05:47 +0200

    ps-4749: create backport

--- src/Spryker/Client/ShoppingList/ShoppingListClientInterface.php
+++ src/Spryker/Client/ShoppingList/ShoppingListClientInterface.php
@@ -289,4 +289,16 @@ interface ShoppingListClientInterface
      * @return \Generated\Shared\Transfer\ShoppingListShareResponseTransfer
      */
     public function updateShoppingListSharedEntities(ShoppingListTransfer $shoppingListTransfer): ShoppingListShareResponseTransfer;
+
+    /**
+     * Specification:
+     *  - Gets customer from session.
+     *  - Makes Zed request. Gets customer by email.
+     *  - Updates customer in session.
+     *
+     * @api
+     *
+     * @return void
+     */
+    public function updateCustomerPermission(): void;
 }
